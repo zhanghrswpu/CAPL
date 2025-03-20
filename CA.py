00d710cc76c115bccedaa55ee95ffbf76117e2b0
zhanghrswpu <68892915+zhanghrswpu@users.noreply.github.com>
2025-03-20 12:43:01 +0800

    Update CA.py

--- CA.py
+++ CA.py
@@ -7,7 +7,6 @@ class CALayer(Layer):
         self.lambda_acr = lambda_acr
 
     def build(self, input_shape):
-        # 初始化中心向量，作为可学习的参数
         self.center = self.add_weight(
             shape=(input_shape[-1],),
             initializer='zeros',
@@ -17,8 +16,6 @@ class CALayer(Layer):
         super(CALayer, self).build(input_shape)
 
     def call(self, inputs):
-        # 正则化项：最小化样本与中心向量的距离
         acr_regularization = self.lambda_acr * tf.reduce_mean(tf.square(inputs - self.center))
-        # 返回输入，便于后续网络处理
         self.add_loss(acr_regularization)
         return inputs-self.center
